78e58538664f81da9e526cee27eefd9a826e537b
AlexeyEsipov <a_esipov@list.ru>
2022-01-07 17:04:10 +0300

    3. Расширить Store для пользователя. [#283109]

--- db/scripts/update_002.sql
+++ db/scripts/update_002.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS users cascade ;
 CREATE TABLE if not exists users (
                                      id SERIAL PRIMARY KEY,
                                      name TEXT,
-                                     email TEXT unique ,
+                                     email varchar(50) unique ,
                                      password TEXT,
                                      created TIMESTAMP DEFAULT now(),
                                      modified TIMESTAMP DEFAULT now()
