55ed05f8ff7a5657319f5b574ae8beb864631ccb
AlexeyEsipov <a_esipov@list.ru>
2022-01-07 11:51:46 +0300

    2. Filter [#2517]

---
+++ src/main/java/ru/job4j/dream/filter/AuthFilter.java
@@ -0,0 +1,24 @@
+package ru.job4j.dream.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class AuthFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest sreq, ServletResponse sresp, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) sreq;
+        HttpServletResponse resp = (HttpServletResponse) sresp;
+        String uri = req.getRequestURI();
+        if (uri.endsWith("auth.do")) {
+            chain.doFilter(sreq, sresp);
+            return;
+        }
+        if (req.getSession().getAttribute("user") == null) {
+            resp.sendRedirect(req.getContextPath() + "/candidate/login.jsp");
+            return;
+        }
+        chain.doFilter(sreq, sresp);
+    }
+}
