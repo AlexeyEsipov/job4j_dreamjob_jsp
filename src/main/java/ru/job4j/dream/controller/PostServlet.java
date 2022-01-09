c5248692789eb761f32166fc82f3e0922d37dbd3
AlexeyEsipov <a_esipov@list.ru>
2022-01-09 16:46:15 +0300

    1. Написать тест на PostServlet. [#2518]

--- src/main/java/ru/job4j/dream/controller/PostServlet.java
+++ src/main/java/ru/job4j/dream/controller/PostServlet.java
@@ -1,7 +1,8 @@
-package ru.job4j.dream.servlet;
+package ru.job4j.dream.controller;
 
 import ru.job4j.dream.model.Post;
 import ru.job4j.dream.store.DbStore;
+import ru.job4j.dream.store.Store;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,21 +12,27 @@ import java.io.IOException;
 
 public class PostServlet extends HttpServlet {
 
+    private final Store store = DbStore.instOf();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("posts", DbStore.instOf().findAllPosts());
-        req.getRequestDispatcher("/post/posts.jsp").forward(req, resp);
+        String edit = req.getParameter("edit");
+        String path = edit != null ? "/post/edit.jsp" : "/post/posts.jsp";
+        req.setAttribute("user", req.getSession().getAttribute("user"));
+        if (edit == null) {
+            req.setAttribute("posts", store.findAllPosts());
+        }
+        req.getRequestDispatcher(path).forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         req.setCharacterEncoding("UTF-8");
-        DbStore.instOf().savePost(
-            new Post(
+        Post post = new Post(
                 Integer.parseInt(req.getParameter("id")),
                 req.getParameter("name")
-            )
         );
+        store.savePost(post);
         resp.sendRedirect(req.getContextPath() + "/posts.do");
     }
 }
